fda4f9f368b77f0534503c96f877e4ec26409dbc
android <android@gmail.com>
2024-07-17 17:12:32 +0700

    icons

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("pages/home.py", title="Home", icon="🏠"),
-    st.Page("pages/document_summary.py", title="Tóm tắt tài liệu", icon=":material/local_offer:"),
-    st.Page("pages/document_answer.py", title="Hỏi đáp về tài liệu", icon=":material/favorite:"),
-    st.Page("pages/stock_analytics.py", title="Phân tích kỹ thuật cổ phiếu", icon=":material/work:"),
-    st.Page("pages/stock_advisor.py", title="Tra cứu thông tin chứng khoán", icon=":material/attach_money:"),
-    st.Page("pages/stock_agent.py", title="Trợ lý chứng khoán", icon=":material/lock_open:"),
+    st.Page("pages/document_summary.py", title="Tóm tắt tài liệu", icon="✍"),
+    st.Page("pages/document_answer.py", title="Hỏi đáp về tài liệu", icon="🙋‍♀️"),
+    st.Page("pages/stock_analytics.py", title="Phân tích kỹ thuật cổ phiếu", icon="🧑‍💻"),
+    st.Page("pages/stock_advisor.py", title="Tra cứu thông tin chứng khoán", icon="🤠"),
+    st.Page("pages/stock_agent.py", title="Trợ lý chứng khoán", icon="👨‍🏫"),
 ])
 pg.run()
