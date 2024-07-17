37fee1cf427f81cab124368a2610228f74bec3c2
Hung Pham <bjzu@mail.ru>
2024-07-17 11:56:18 +0700

    update side br

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pg = st.navigation([
     st.Page("pages/document_summary.py", title="Tóm tắt tài liệu", icon=":material/local_offer:"),
     st.Page("pages/document_answer.py", title="Hỏi đáp về tài liệu", icon=":material/favorite:"),
     st.Page("pages/stock_analytics.py", title="Phân tích kỹ thuật cổ phiếu", icon=":material/work:"),
-    st.Page("pages/stock_advisor.py", title="Tư vấn chứng khoán", icon=":material/attach_money:"),
+    st.Page("pages/stock_advisor.py", title="Tra cứu thông tin chứng khoán", icon=":material/attach_money:"),
     st.Page("pages/stock_agent.py", title="Trợ lý chứng khoán", icon=":material/lock_open:"),
 ])
 pg.run()
